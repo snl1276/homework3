7b9727c5c2619ad9f71fa2c4f05e50ec69f08cf4
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-02-07 22:59:04 +0300

    changed file zadanie_2.py

--- zadanie_2.py
+++ zadanie_2.py
@@ -4,4 +4,14 @@ print('b=')
 b = input()
 print('c=')
 c = input()
-print(type(a))
+a = int(a)
+b = int(b)
+c = int(c)
+d = (a**2+b**2)/(3*b-4)
+e = 4*c**5/6
+print('Целая часть:')
+div = d//e
+print(div)
+print('Остаток от деления:')
+mod = d % e
+print(mod)
